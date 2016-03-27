a92d59c1d52a423fe7e50f14331c18fb3e9d5df5
Qwant50 <qwantonline@gmail.com>
2016-03-27 20:43:26 +0300

    fix firewall

--- app/models/Message.php
+++ app/models/Message.php
@@ -13,11 +13,12 @@ class Message extends Model
         $stmt = $db->getConnection()->prepare("SELECT message.message_id, message.content, user.login, message.created_at
                                                FROM message LEFT JOIN user ON (message.user_id = user.user_id)
                                                WHERE message.created_at >= DATE_SUB(NOW(),INTERVAL 2 HOUR)");
+        $messages = [];
         $stmt->execute();
         while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
             $messages[] = $row;
         }
-        return $messages ? : [];
+        return $messages;
     }
 
     public function saveMessage($login, $content)
